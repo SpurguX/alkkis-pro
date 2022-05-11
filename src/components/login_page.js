44bf2fa0026dbe4640d4c419cd64ab603965c3ce
Taina Jesse <jesse.taina@hiq.fi>
2022-05-11 16:56:09 +0300

    Fix react versions. Initial login page changes.

--- src/components/login_page.js
+++ src/components/login_page.js
@@ -13,38 +13,38 @@ export default class LoginPage extends Component {
 
     render() {
         return (
-            <div id="main" className="container">
+            <div id="main" className="container-fluid container-login">
                 <div id="banner">
-                    <h1>ALKKIS PRO</h1>
+                    <h1>ALKKIS PRO</h1> 
                 </div>
-                <div className="content">
+                <div className="container content">
                     <div id="kirjautuminen">
                         <form className="form-horizontal" action="/in">
                             <div className="form-group">
-                                <label className="control-label col-sm-2 col-sm-offset-2"><span className="glyphicon glyphicon-user login-icon"></span></label>
-                                <div className="col-sm-4" >
-                                    <input type="text" className="form-control input-lg" placeholder="Käyttäjänimi" />
+                                {/* <label className="control-label col-sm-2 col-sm-offset-2"><span className="glyphicon glyphicon-user login-icon"></span></label> */}
+                                <div>
+                                    <input type="text" className="form-control form-control-lg" placeholder="Käyttäjänimi" />
                                 </div>
                             </div>
                             <div className="form-group">
-                                <label className="control-label col-sm-2 col-sm-offset-2" ><span className="glyphicon glyphicon-lock login-icon"></span></label>
-                                <div className="col-sm-4" >
-                                    <input type="password" className="form-control input-lg" placeholder="Salasana" />
+                                {/* <label className="control-label col-sm-2 col-sm-offset-2" ><span className="glyphicon glyphicon-lock login-icon"></span></label> */}
+                                <div>
+                                    <input type="password" className="form-control form-control-lg" placeholder="Salasana" />
                                 </div>
                             </div>
                             <div className="form-group">
-                                <div className="col-sm-4 col-sm-offset-4">
+                                <div>
                                 <input type="submit" className="btn btn-primary btn-block btn-lg" value="KIRJAUDU" />
                                 </div>
                             </div>
                         </form>
                     </div>
                     <div>
-                        <div id="kirjautumissivu-kuva" className="col-sm-6">
-                            <img src={beer} alt="beer" style={{height: '30rem'}}/>
-                        </div>
+                        {/* <div id="kirjautumissivu-kuva">
+                            <img src={beer} alt="beer" style={{width: '100%'}}/>
+                        </div> */}
                         <div id="kirjautumissivu-kuvaus" className="col-sm-6">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <p>Nyt vasta aletaan ryyppäämään!</p>
                         </div>
                     </div>
                 </div>
