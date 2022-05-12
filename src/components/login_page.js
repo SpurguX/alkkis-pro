09539bed6d617c1f24e71787d4ee41d4a96d7c8c
Taina Jesse <jesse.taina@hiq.fi>
2022-05-12 11:48:44 +0300

    Improve appearance of login page

--- src/components/login_page.js
+++ src/components/login_page.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import beer from '../images/craft-beer.jpg';
+import logo from '../images/logo.png';
 
 export default class LoginPage extends Component {
     constructor(props) {
@@ -13,38 +14,33 @@ export default class LoginPage extends Component {
 
     render() {
         return (
-            <div id="main" className="container-fluid container-login">
-                <div id="banner">
-                    <h1>ALKKIS PRO</h1> 
-                </div>
-                <div className="container content">
-                    <div id="kirjautuminen">
-                        <form className="form-horizontal" action="/in">
-                            <div className="form-group">
-                                {/* <label className="control-label col-sm-2 col-sm-offset-2"><span className="glyphicon glyphicon-user login-icon"></span></label> */}
-                                <div>
-                                    <input type="text" className="form-control form-control-lg" placeholder="Käyttäjänimi" />
-                                </div>
-                            </div>
-                            <div className="form-group">
-                                {/* <label className="control-label col-sm-2 col-sm-offset-2" ><span className="glyphicon glyphicon-lock login-icon"></span></label> */}
-                                <div>
-                                    <input type="password" className="form-control form-control-lg" placeholder="Salasana" />
-                                </div>
-                            </div>
-                            <div className="form-group">
-                                <div>
-                                <input type="submit" className="btn btn-primary btn-block btn-lg" value="KIRJAUDU" />
-                                </div>
-                            </div>
-                        </form>
+            <div id="main" className="container-fluid container-login px-0">
+                <div className="container login-wrapper d-flex flex-column justify-content-center">
+                    <div className="mb-4 logo--login--wrapper rounded">
+                        <img src={logo} className="logo--login" />
                     </div>
-                    <div>
-                        {/* <div id="kirjautumissivu-kuva">
-                            <img src={beer} alt="beer" style={{width: '100%'}}/>
-                        </div> */}
-                        <div id="kirjautumissivu-kuvaus" className="col-sm-6">
-                            <p>Nyt vasta aletaan ryyppäämään!</p>
+                    <div className="content">
+                        <div id="kirjautuminen">
+                            <form className="form-horizontal" action="/in">
+                                <div className="form-group">
+                                    <div>
+                                        <input type="text" className="form-control form-control-lg" placeholder="Käyttäjänimi" />
+                                    </div>
+                                </div>
+                                <div className="form-group">
+                                    <div>
+                                        <input type="password" className="form-control form-control-lg" placeholder="Salasana" />
+                                    </div>
+                                </div>
+                                <div className="form-group">
+                                    <div>
+                                    <input type="submit" className="btn btn-info btn-block btn-lg" value="KIRJAUDU" />
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div id="kirjautumissivu-kuvaus" className="text-center">
+                            Nyt vasta aletaan ryyppäämään!
                         </div>
                     </div>
                 </div>
