a5ad07065502e45ee1639e403d0ee567aea23a87
Taina Jesse <jesse.taina@hiq.fi>
2022-06-17 15:56:16 +0300

    WIP: Layout updates. Style datepicker. Add date-fns to get locale for datepicker. Run prettier formatting for index.scss.

--- src/components/drink_datepicker.js
+++ src/components/drink_datepicker.js
@@ -2,10 +2,12 @@ import React, { Component, useState } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { updateDrinkDate } from "../actions";
-import DatePicker from "react-datepicker";
+import DatePicker, { registerLocale } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "../datepicker.css";
-import "moment/locale/fi";
+import fi from "date-fns/locale/fi";
+
+registerLocale(fi)
 
 class DrinkDatePicker extends Component {
   handleChange(date) {
@@ -22,10 +24,56 @@ class DrinkDatePicker extends Component {
         <DatePicker
           selected={this.props.drinkDate}
           onChange={date => this.handleChange(date)}
-          locale="fi"
+          dateFormat="d.M.yyyy"
+          locale={fi}
+          renderCustomHeader={({
+            monthDate,
+            decreaseMonth,
+            increaseMonth,
+          }) => (
+            <div>
+              <button
+                aria-label="Previous Month"
+                className={
+                  "react-datepicker__navigation react-datepicker__navigation--previous"
+                }
+                onClick={decreaseMonth}
+              >
+                <span
+                  className={
+                    "react-datepicker__navigation-icon react-datepicker__navigation-icon--previous"
+                  }
+                >
+                  {"<"}
+                </span>
+              </button>
+              <span className="react-datepicker__current-month">
+                {monthDate.toLocaleString("fi-FI", {
+                  month: "long",
+                  // year: "numeric",
+                })}
+                <span className="font-segoe font-weight-light"> {monthDate.toLocaleString("fi-FI", { year: "numeric"})}</span>
+              </span>
+              <button
+                aria-label="Next Month"
+                className={
+                  "react-datepicker__navigation react-datepicker__navigation--next"
+                }
+                onClick={increaseMonth}
+              >
+                <span
+                  className={
+                    "react-datepicker__navigation-icon react-datepicker__navigation-icon--next"
+                  }
+                >
+                  {">"}
+                </span>
+              </button>
+            </div>
+          )}
         />
-      </div>
-    );
+        </div>
+      );
   }
 }
 
