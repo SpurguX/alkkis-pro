6905e0916d8397e877d3e3952c967f3630a3b13b
Taina Jesse <jesse.taina@hiq.fi>
2022-08-10 15:48:24 +0300

    Use production url.

--- src/network/axiosApi.js
+++ src/network/axiosApi.js
@@ -3,7 +3,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080'
+  // baseURL: 'http://localhost:8080'
+  // TODO get url from env config
+  // TODO update digitalocean droplet to a newer Ubuntu version in order to use https
+  baseURL: 'http://jessetaina.info:8080'
 });
 
 export default api;
